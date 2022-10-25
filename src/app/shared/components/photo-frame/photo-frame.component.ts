03c5df8580c4545c4f11a5b42e501b86938f80f6
Anderson Silva <andssilva.23@gmail.com>
2022-10-24 23:08:42 -0400

    feat: :rocket: criando um observable para aguardar um intervalo de tempo entre cliques

--- src/app/shared/components/photo-frame/photo-frame.component.ts
+++ src/app/shared/components/photo-frame/photo-frame.component.ts
@@ -1,24 +1,43 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { Subject } from 'rxjs';
+import { debounceTime, takeUntil } from 'rxjs/operators'
 
 @Component({
   selector: 'app-photo-frame',
   templateUrl: './photo-frame.component.html',
   styleUrls: ['./photo-frame.component.scss']
 })
-export class PhotoFrameComponent implements OnInit {
+export class PhotoFrameComponent implements OnInit, OnDestroy {
 
   @Output() public liked: EventEmitter<void> = new EventEmitter();
   @Input() public description: string = "";
   @Input() public src: string = "";
   @Input() public likes: number = 0;
 
+  private debounceSubject: Subject<void> = new Subject();
+  private unsubscribe: Subject<void> = new Subject();
+
   constructor() { }
 
-  ngOnInit(): void {
+  public ngOnInit(): void {
+    this.debounceSubject
+      .asObservable()
+      .pipe(
+        debounceTime(500),
+        takeUntil(this.unsubscribe)
+      )
+      .subscribe(
+        () => this.liked.emit()
+      )
   }
 
   public like(): void {
-    this.liked.emit();
+    this.debounceSubject.next();
+  }
+
+  public ngOnDestroy(): void {
+    this.unsubscribe.next();
+    this.unsubscribe.complete();
   }
 
 }
