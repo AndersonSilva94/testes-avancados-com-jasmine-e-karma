a775ebac1d2fbf88c20c75b211edecedf0a4fe37
Anderson Silva <andssilva.23@gmail.com>
2022-10-28 01:42:29 -0400

    feat: :rocket: criando testes com fakeAsync e tick

--- src/app/shared/components/photo-frame/photo-frame.component.spec.ts
+++ src/app/shared/components/photo-frame/photo-frame.component.spec.ts
@@ -1,4 +1,9 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  ComponentFixture,
+  fakeAsync,
+  TestBed,
+  tick,
+} from '@angular/core/testing';
 
 import { PhotoFrameComponent } from './photo-frame.component';
 import { PhotoFrameModule } from './photo-frame.module';
@@ -9,9 +14,8 @@ describe(PhotoFrameComponent.name, () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [PhotoFrameModule]
-    })
-    .compileComponents();
+      imports: [PhotoFrameModule],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(PhotoFrameComponent);
     component = fixture.componentInstance;
@@ -20,4 +24,28 @@ describe(PhotoFrameComponent.name, () => {
   it('Should create component', () => {
     expect(component).toBeTruthy();
   });
+
+  it(`#${PhotoFrameComponent.prototype.like.name} should trigger (@Output liked) once when called multiple times within debounce time`, fakeAsync(() => {
+    // fakeAsync gera um espaço onde é possível criar um tempo de espera pra função ocorrer
+    fixture.detectChanges();
+    let times = 0;
+    component.liked.subscribe(() => times++);
+    component.like();
+    component.like();
+
+    tick(500);
+    // tick é o método onde passamos o tempo que precisamos esperar para a função ocorrer
+    expect(times).toBe(1);
+  }));
+
+  it(`#${PhotoFrameComponent.prototype.like.name} should trigger (@output liked) two times when called outside debounce time`, fakeAsync(() => {
+    fixture.detectChanges();
+    let times = 0;
+    component.liked.subscribe(() => times++);
+    component.like();
+    tick(500);
+    component.like();
+    tick(500);
+    expect(times).toBe(2);
+  }));
 });
