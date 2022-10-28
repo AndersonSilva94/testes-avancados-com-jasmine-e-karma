73ad8795519160719bf803ed6a5548960735c2bb
Anderson Silva <andssilva.23@gmail.com>
2022-10-28 02:12:06 -0400

    feat: :rocket: testando imagem e inserindo flags de teste em DOM

--- src/app/shared/components/photo-frame/photo-frame.component.spec.ts
+++ src/app/shared/components/photo-frame/photo-frame.component.spec.ts
@@ -49,7 +49,7 @@ describe(PhotoFrameComponent.name, () => {
     expect(times).toBe(2);
   }));
 
-  it(`#${PhotoFrameComponent.name} should display number of likes when (@Input likes) is incremented`, () => {
+  it(`(D) #${PhotoFrameComponent.name} should display number of likes when (@Input likes) is incremented`, () => {
     fixture.detectChanges();
     component.likes++;
     fixture.detectChanges();
@@ -57,7 +57,7 @@ describe(PhotoFrameComponent.name, () => {
     expect(element.textContent.trim()).toBe('1')
   });
 
-  it(`Should update aria-label when (@Input likes) is incremented`, () => {
+  it(`(D) Should update aria-label when (@Input likes) is incremented`, () => {
     fixture.detectChanges();
     component.likes++;
     fixture.detectChanges();
@@ -65,9 +65,22 @@ describe(PhotoFrameComponent.name, () => {
     expect(element.getAttribute('aria-label')).toBe('1: people liked')
   });
 
-  it(`Should have aria-label with 0 (@Input likes)`, () => {
+  it(`(D) Should have aria-label with 0 (@Input likes)`, () => {
     fixture.detectChanges();
     const element: HTMLElement = fixture.nativeElement.querySelector('span');
     expect(element.getAttribute('aria-label')).toBe('0: people liked')
+  });
+
+  it(`(D) Should display image with src and description when bound to properties`, () => {
+    const description = 'some description';
+    const src = 'http://somesite.com/img.jpg'
+    component.src = src;
+    component.description = description;
+
+    fixture.detectChanges();
+    const img: HTMLImageElement = fixture.nativeElement.querySelector('img');
+
+    expect(img.getAttribute('src')).toBe(src);
+    expect(img.getAttribute('alt')).toBe(description);
   })
 });
