6c98af9e38a4d0bb9a3a217e4f1f81af973f3348
Anderson Silva <andssilva.23@gmail.com>
2022-11-08 22:39:33 -0400

    feat: :rocket: renderizando o componente photo board

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Photo } from './shared/components/photo-board/interfaces/photo';
+import { PhotoBoardService } from './shared/components/photo-board/services/photo-board.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Angular testing';
-  public likes = 0;
 
-  public like(): void {
-    this.likes++;
+  public photos$: Observable<Photo[]>;
+
+  constructor(
+    private service: PhotoBoardService
+  ) {}
+
+  public ngOnInit(): void {
+    this.photos$ = this.service.getPhotos();
   }
 }
