2bd7fd09d181777e2c33b3e4dd5df760964c513f
Anderson Silva <andssilva.23@gmail.com>
2022-11-13 01:28:44 -0400

    feat: :rocket: adicionando loading na aplicação

--- src/app/components/photo-list/photo-list.module.ts
+++ src/app/components/photo-list/photo-list.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PhotoListComponent } from './photo-list.component';
 import { PhotoBoardModule } from 'src/app/shared/components/photo-board/photo-board.module';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 
 @NgModule({
   declarations: [PhotoListComponent],
   imports: [
     CommonModule,
-    PhotoBoardModule
+    PhotoBoardModule,
+    FontAwesomeModule
   ],
   exports: [PhotoListComponent]
 })
--- src/app/shared/components/photo-board/services/photo-board.service.ts
+++ src/app/shared/components/photo-board/services/photo-board.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { delay } from 'rxjs/operators';
 import { Photo } from '../interfaces/photo';
 
 @Injectable({
@@ -14,5 +15,6 @@ export class PhotoBoardService {
 
   public getPhotos(): Observable<Photo[]> {
     return this.http.get<Photo[]>('http://localhost:3000/photos')
+      .pipe(delay(2000));
   }
 }
