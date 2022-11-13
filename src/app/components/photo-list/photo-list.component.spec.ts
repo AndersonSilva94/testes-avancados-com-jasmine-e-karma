658a64c5acfa749fb48eb3436ed6d82344fe0a7e
Anderson Silva <andssilva.23@gmail.com>
2022-11-13 01:49:31 -0400

    feat: :rocket: injetando o serviço de PhotoBoard no teste da página

--- src/app/components/photo-list/photo-list.component.spec.ts
+++ src/app/components/photo-list/photo-list.component.spec.ts
@@ -1,11 +1,13 @@
 import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { PhotoBoardService } from 'src/app/shared/components/photo-board/services/photo-board.service';
 import { PhotoListComponent } from './photo-list.component';
 import { PhotoListModule } from './photo-list.module';
 
 describe(PhotoListComponent.name, () => {
   let fixture: ComponentFixture<PhotoListComponent>;
   let component: PhotoListComponent;
+  let service: PhotoBoardService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -18,9 +20,15 @@ describe(PhotoListComponent.name, () => {
 
     fixture = TestBed.createComponent(PhotoListComponent);
     component = fixture.componentInstance;
+    service = TestBed.inject(PhotoBoardService);
   });
 
   it('Should create component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it(`(D) Should display board when data arrives`, () => {
+    fixture.detectChanges();
+
   })
 });
